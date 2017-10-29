509a886ded236c81e1a2c3f205d159bbfccb4f94
BrunoWozniak <bruno.wozniak@gmail.com>
2017-10-29 20:58:33 +0100

    Added Stripe and credit purchases

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
     renderContent() {
@@ -9,15 +10,18 @@ class Header extends Component {
                 return;
             case false:
                 return(
-                    <li>
-                        <a href="/auth/google">Login with Google</a>
-                    </li>
+                    <li><a href="/auth/google">Login with Google</a></li>
                 );
             default:
+                const creditWord = this.props.auth.credits > 1 ? 'credits' : 'credit';
                 return(
-                    <li>
-                        <a href="/api/logout">Logout</a>
-                    </li>
+                    [
+                        <li key="1"><Payments /></li>,
+                        <li key="2" style={{ margin: '0 10px' }}>
+                            {this.props.auth.credits} {creditWord}
+                        </li>,
+                        <li key="3"><a href="/api/logout">Logout</a></li>
+                    ]
                 ); 
         }
     }
