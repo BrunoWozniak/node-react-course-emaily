468e4d95e8844dff68167b2e97cb2a2a98066f0c
BrunoWozniak <bruno.wozniak@gmail.com>
2017-10-30 23:50:38 +0100

    Added backend for surveys, including SendGrid integration

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,11 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 
+// Dev only, axios helper...
+import axios from 'axios';
+window.axios = axios;
+
+
 export default combineReducers({
     auth: authReducer
 });
